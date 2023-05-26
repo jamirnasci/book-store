0c6012704c929e6ce5dae7dad3e783b20580618c
Jamir Junior <89231614+jamirnasci@users.noreply.github.com>
2023-05-26 15:12:01 -0600

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,43 @@
+const sideMenuBtn = document.getElementById('side-menu-btn')
+const optionBtns = document.getElementsByClassName('side-option-btn')
 
+sideMenuBtn.addEventListener('click', ()=>{
+    $('#side-menu').fadeToggle(500)
+})
+
+$('#back-button').click(function(){
+    $('#side-menu').fadeToggle(500)
+})
+
+function getBooks(category, categoryTitle){
+    $('#book-box').empty()
+    $('#category-title').text(categoryTitle)
+    $.ajax({
+        method:"GET",
+        url:"/getbook",
+        data:{
+            type:category
+        },
+        success:function(data){
+            let jsonB = JSON.parse(data)
+            for(jsonObj of jsonB){
+                let newBook = `
+                    <div class="book-card">
+                        <img src="${jsonObj['imgurl']}" alt="book img">
+                        <h2 class="book-title">${jsonObj['title']}</h2>
+                        <p>${jsonObj['author']}</p>
+                        <b>${jsonObj['price']}</b>
+                        <button class="buy-btn">Buy</button>
+                    </div>
+                        `
+                $('#book-box').append(newBook)
+            }
+        }
+    })
+}
+
+for(let i = 0; i < optionBtns.length; i++){
+    optionBtns[i].addEventListener('click', function(tag) {
+        getBooks(this.getAttribute('cat'), this.innerHTML)        
+    })
+}
