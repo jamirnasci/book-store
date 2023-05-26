75476fd4a504e51456cb2c9465af1d3d6baa8f46
Jamir Junior <89231614+jamirnasci@users.noreply.github.com>
2023-05-26 15:10:29 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const fs = require('fs/promises')
+
+app.use(express.static("public"))
+const port = 3333
+
+async function readJson(){
+    const fileString = await fs.readFile("./data.json", {encoding:'utf8'})
+    return String(fileString)
+}
+
+app.get('/getbook', (req, res)=>{
+    let type = req.query.type
+    let jsonObj = ''
+    let jsonStringResponse = ''
+
+    readJson().then((objString) => { 
+        jsonObj = JSON.parse(objString)
+        jsonStringResponse = jsonObj[type]
+        res.send(JSON.stringify(jsonStringResponse))
+    })
+    
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+"\index.html")
+})
+
+app.listen(port, ()=>{console.log('Running on '+port)})
